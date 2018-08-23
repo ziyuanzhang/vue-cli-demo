d2a72079998d27cef865d7f86b971534b1e7e6eb
ziyuanzhang <2661548928@qq.com>
2018-08-23 16:50:52 +0800

    youhua

--- src/components/home.vue
+++ src/components/home.vue
@@ -3,6 +3,9 @@
         <h1>我是 home--home</h1>
         <hr>
         <button @click="addAttrFun()">data添加属性</button>
+        <p>history 模式: 发布后history 模式刷新会有404问题，解决办法需要后端配置 <br />
+          https://router.vuejs.org/zh/guide/essentials/history-mode.html#%E5%90%8E%E7%AB%AF%E9%85%8D%E7%BD%AE%E4%BE%8B%E5%AD%90
+          </p>
     </div>
 </template>
 <script>
