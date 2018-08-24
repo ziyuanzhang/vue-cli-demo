17e5b0f579cdeeba8a990f648d6c856f366496e8
ziyuanzhang <2661548928@qq.com>
2018-08-24 09:37:44 +0800

    youhua

--- src/components/home.vue
+++ src/components/home.vue
@@ -4,8 +4,8 @@
         <hr>
         <p class="attr-show">{{persen}}</p>
         <button @click="addAttrFun()">data添加属性</button>
-        <p>history 模式: 发布后history 模式刷新会有404问题，解决办法需要后端配置 <br />
-          https://router.vuejs.org/zh/guide/essentials/history-mode.html#%E5%90%8E%E7%AB%AF%E9%85%8D%E7%BD%AE%E4%BE%8B%E5%AD%90
+        <p>history 模式: 发布后history 模式刷新会有404问题，解决办法 <a href="https://router.vuejs.org/zh/guide/essentials/history-mode.html#%E5%90%8E%E7%AB%AF%E9%85%8D%E7%BD%AE%E4%BE%8B%E5%AD%90" target="_blank">后端配置</a>  <br />
+
           </p>
     </div>
 </template>
@@ -33,5 +33,9 @@ export default {
     }
     .attr-show{
        margin: 10px 0;
+    }
+    .home a{
+      color: red;
+      border-bottom: 1px solid red;
     }
 </style>
